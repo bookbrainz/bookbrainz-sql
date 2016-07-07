b4066b0e96a2ccfff2c6690c100900b4bc50ebaf
Max Prettyjohns <heshoots9999@gmail.com>
2016-07-07 16:11:15 +0100

    Add editor_referral table to schema

--- schemas/bookbrainz.sql
+++ schemas/bookbrainz.sql
@@ -609,6 +609,14 @@ CREATE TABLE bookbrainz._editor_entity_visits (
 ALTER TABLE bookbrainz._editor_entity_visits ADD FOREIGN KEY (editor_id) REFERENCES bookbrainz.editor (id);
 ALTER TABLE bookbrainz._editor_entity_visits ADD FOREIGN KEY (bbid) REFERENCES bookbrainz.entity (bbid);
 
+CREATE TABLE bookbrainz._editor_referral (
+	id SERIAL PRIMARY KEY,
+	recruiter_id INT NOT NULL,
+	editor_id INT NOT NULL
+);
+ALTER TABLE bookbrainz._editor_referral ADD FOREIGN KEY (recruiter_id) REFERENCES bookbrainz.editor (id);
+ALTER TABLE bookbrainz._editor_referral ADD FOREIGN KEY (editor_id) REFERENCES bookbrainz.editor (id);
+
 -- Views --
 
 CREATE VIEW bookbrainz.creator AS
